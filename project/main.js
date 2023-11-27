5241999add543f55d6a4b95738be1abc168726fc
TyYipp <144461219+TyYipp@users.noreply.github.com>
2023-11-27 10:13:30 -0500

    11/27
    trying to filter

--- project/main.js
+++ project/main.js
@@ -2,9 +2,18 @@ import './style.css'
 import { troops } from './array.js'
 
 const DOMSelectors = {
-  parent: document.getElementById("parent")
+  parent: document.getElementById("parent"),
+  dark: document.getElementById("dark"),
+  normal: document.getElementById("normal"),
+  melee: document.getElementById("melee"),
+  ranged: document.getElementById("ranged"),
+  defenses: document.getElementById("defenses"),
+  any: document.getElementById("any"),
+  super: document.getElementById("super"),
+  ground: document.getElementById("ground"),
+  air: document.getElementById("air"),
 }
-
+function fill (){
  troops.forEach(troop  => {
  
 	 DOMSelectors.parent.insertAdjacentHTML('afterbegin',  `
@@ -18,8 +27,9 @@ const DOMSelectors = {
   <h7 id="text">Super: ${troop.super}</h7>
   </div>
   `);
-}) 
-event.preventDefault();
+})
+} 
+fill()
 
 
     
